f93ebf80e9d00a59ca526dfd0a629e1056440f08
Nishant Kumar <nishant4498@gmail.com>
2016-04-17 19:47:53 -0700

    Fixed dictionary issue and changed output for calculating f1 score

--- ResearchProject/src/marathiClassify.py
+++ ResearchProject/src/marathiClassify.py
@@ -47,7 +47,7 @@ def populate_hindi_model():
         hindi_model = json.load(fileopen)
 
 def poulate_dictionary():
-    with open("../model/Dictionary.json",'r') as dictopen:
+    with open("../model/Dictionary.txt",'r') as dictopen:
         global hindi_marathi_dict
         hindi_marathi_dict = json.load(dictopen)
 
@@ -68,14 +68,14 @@ def replace_punctuations(word):
     for i in range (0,len(word)):
         if word[i] in punctuations:
             truncated_word  =   re.sub(word[i] , "", truncated_word)
-    if '?' in word[i]:
-        truncated_word=truncated_word.replace("?","")
-    if '(' in word[i]:
-        truncated_word=truncated_word.replace("(","")
-    if ')' in word[i]:
-        truncated_word=truncated_word.replace(")","")
-    if '|' in word[i]:
-        truncated_word=truncated_word.replace("|","")
+        if '?' in word[i]:
+            truncated_word=truncated_word.replace("?","")
+        if '(' in word[i]:
+            truncated_word=truncated_word.replace("(","")
+        if ')' in word[i]:
+            truncated_word=truncated_word.replace(")","")
+        if '|' in word[i]:
+            truncated_word=truncated_word.replace("|","")
     return truncated_word
 
 def initialize_model():
@@ -223,7 +223,8 @@ def create_output_map(path,news_category):
 def write_output_to_file():
     global news_classify
     with open("../output/Classified Marathi News.txt",'a+') as fileopen:
-        json.dump(news_classify,fileopen)
+        for path, classification in news_classify.iteritems():
+            fileopen.write(path+','+path.split('/')[2].split('\\')[0]+','+classification+'\n')
 
 def classify(test_data_path):
     for root, dirs, files in os.walk(test_data_path):
